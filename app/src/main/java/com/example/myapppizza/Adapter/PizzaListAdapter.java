666d6d902ae3b117efe217e14d69a227ccb36a76
501-11 <kjd1374@naver.com>
2019-04-28 16:25:31 +0900

    핏자 리스트

--- app/src/main/java/com/example/myapppizza/Adapter/PizzaListAdapter.java
+++ app/src/main/java/com/example/myapppizza/Adapter/PizzaListAdapter.java
@@ -5,8 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.myapppizza.PizzaListItem;
 import com.example.myapppizza.R;
 
@@ -19,7 +22,7 @@ public class PizzaListAdapter extends ArrayAdapter<PizzaListItem> {
     LayoutInflater inf;
 
     public PizzaListAdapter (Context context, List<PizzaListItem> list) {
-        super(context, R.layout.activity_piza_list, list);
+        super(context, R.layout.activity_pizza_list, list);
 
         mContext = context;
         mList = list;
@@ -31,7 +34,7 @@ public class PizzaListAdapter extends ArrayAdapter<PizzaListItem> {
         View row = ConvertView;
 
         if (row == null) {
-            row = inf.inflate(R.layout.activity_piza_list, null);
+            row = inf.inflate(R.layout.activity_pizza_list, null);
         }
 
         PizzaListItem appData = mList.get(position);
@@ -39,6 +42,21 @@ public class PizzaListAdapter extends ArrayAdapter<PizzaListItem> {
         TextView storeNameText = row.findViewById(R.id.storeNameText);
 
         storeNameText.setText(appData.storeName);
+
+        ImageView storeLogoImage = row.findViewById(R.id.store_logo_image);
+
+        String url = appData.logoImgUrl;
+
+        Glide.with(mContext).load(url).into(storeLogoImage);
+
+        ImageView detailImageView = row.findViewById(R.id.detailImageView);
+
+        detailImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "으앎아ㅓ", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return  row;
     }
--- app/src/main/java/com/example/myapppizza/MainActivity.java
+++ app/src/main/java/com/example/myapppizza/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapppizza;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
 import com.example.myapppizza.Adapter.PizzaListAdapter;
 import com.example.myapppizza.databinding.ActivityMainBinding;
 
@@ -31,6 +33,7 @@ public class MainActivity extends BaseActivity {
         act.orderListView.setAdapter(mAppadapter);
 
         GetpizzaList();
+
     }
 
     @Override
@@ -49,10 +52,12 @@ public class MainActivity extends BaseActivity {
     }
 
     void GetpizzaList() {
-        plist.add(new PizzaListItem("피자헛", "09:00 ~ 12:00", "https://www.google.com/search?q=%ED%94%BC%EC%9E%90%ED%97%9B%EB%A1%9C%EA%B3%A0&tbm=isch&source=iu&ictx=1&fir=zN4qxcGGbAdKmM%253A%252CCtx4hXhDX1BUSM%252C_&vet=1&usg=AI4_-kSyruYhQaEIWkID2wF0PBZryOla6g&sa=X&ved=2ahUKEwizwpzvkPLhAhVQBKYKHYHLD-0Q9QEwCXoECAcQFg#imgrc=zN4qxcGGbAdKmM:"));
-        plist.add(new PizzaListItem("미스터피자", "08:40 ~ 16:00", "https://www.google.com/search?q=%EB%AF%B8%EC%8A%A4%ED%84%B0%ED%94%BC%EC%9E%90+%EB%A1%9C%EA%B3%A0&tbm=isch&source=iu&ictx=1&fir=Q16n8IUAwG-gwM%253A%252CGaRCnIb_Y1O_lM%252C_&vet=1&usg=AI4_-kS6gGvoER-mLOQxcM-QLW7A-KXYmA&sa=X&ved=2ahUKEwjHquCHkfLhAhUYyYsBHVdlDu0Q9QEwA3oECAcQCg#imgrc=Q16n8IUAwG-gwM:"));
-        plist.add(new PizzaListItem("파파존슨", "07:00 ~ 19:22", "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjA7qO2kfLhAhXZxYsBHaQlDPMQjRx6BAgBEAU&url=https%3A%2F%2Fpl-pl.facebook.com%2Fpages%2Fcategory%2FPizza-Place%2Fpapasejong%2Fposts%2F&psig=AOvVaw2Sg6d5GdJUQ9fHYBcM2iWx&ust=1556518505354439"));
-        plist.add(new PizzaListItem("도미노피자", "09:32 ~ 12:23", "http://image.news1.kr/system/photos/2013/3/26/415832/medium.jpg"));
-        plist.add(new PizzaListItem("임실피자", "10:25 ~ 18:55", "http://www.jjan.kr/news/photo/201202/428246_112478_2716.jpg"));
+        plist.add(new PizzaListItem("파파존스", "09:00~21:00", "http://postfiles2.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png"));
+        plist.add(new PizzaListItem("피자헛", "09:00~21:00", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png"));
+        plist.add(new PizzaListItem("도미노피자", "09:00~21:00", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/120px-Domino%27s_pizza_logo.svg.png"));
+        plist.add(new PizzaListItem("미스터피자", "09:00~21:00", "http://postfiles12.naver.net/20160530_171/ppanppane_14646177044221JRNd_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png"));
+        plist.add(new PizzaListItem("피자에땅", "09:00~21:00", "https://mblogthumb-phinf.pstatic.net/20160530_214/ppanppane_1464617804922knKn4_PNG/%C7%C7%C0%DA%BF%A1%B6%A5_%B7%CE%B0%ED_%282%29.png"));
+        plist.add(new PizzaListItem("피자스쿨", "09:00~21:00", "https://modo-phinf.pstatic.net/20150501_269/1430484184544WKwLF_JPEG/mosa7NPaR2.jpeg"));
+        plist.add(new PizzaListItem("피자나라 치킨공주", "09:00~21:00", "http://www.newsworker.co.kr/news/photo/201810/22592_19806_5940.jpg"));
     }
 }
